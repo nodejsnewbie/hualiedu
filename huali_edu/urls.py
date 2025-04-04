8646d7db8e6db761c3c0a76285610c7a25e8607e
Your Name <you@example.com>
2025-04-04 15:04:46 +0800

    删除多余目录

--- huali_edu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from grading.admin import admin_site
-
-urlpatterns = [
-    path('admin/', admin_site.urls),
-    path('grading/', include('grading.urls')),
-] 
